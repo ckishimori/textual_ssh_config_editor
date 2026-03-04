7391d26c29371b5f474a88147f3180978edb8eef
ckishimori <ckishimori@gmail.com>
2026-03-03 17:38:17 -1000

    tinkering

--- app.py
+++ app.py
@@ -11,15 +11,19 @@ from textual.widgets import (
 class SSHConfigEditor(App):
     def compose(self):
         yield Header()
-        yield Input(placeholder="Host")
-        yield Input(placeholder="Hostname")
-        yield Input(placeholder="Username")
-        yield Input(placeholder="Port")
-        yield Button("Add")
+        yield Input(placeholder="Host", id="host_field")
+        yield Input(placeholder="Hostname", id="hostname_field")
+        yield Input(placeholder="Username", id="username_field")
+        yield Input(placeholder="Port", id="port_field")
+        yield Button("Add", id="add_button")
         yield Footer()
 
     def on_mount(self):
         self.title = "Textual SSH config editor"
+
+    def on_button_pressed(self, event):
+        if event.button.id == "add_button":
+            print(self.query_one("#host_field").value)
 
 def main():
     app = SSHConfigEditor()
