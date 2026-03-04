ddca99b6f6d563af3d063fa302e8ddd4c3224430
ckishimori <ckishimori@gmail.com>
2026-03-03 20:17:16 -1000

    add to config working

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ from textual.widgets import (
 )
 
 class SSHConfigEditor(App):
+
+    def __init__(self, conf):
+        super().__init__()
+        self.conf = conf
+
     def compose(self):
         yield Header()
         yield Input(placeholder="Host", id="host_field")
@@ -23,10 +28,17 @@ class SSHConfigEditor(App):
 
     def on_button_pressed(self, event):
         if event.button.id == "add_button":
-            print(self.query_one("#host_field").value)
+            self.conf.add(
+                self.query_one("#host_field").value,
+                HostName=self.query_one("#hostname_field").value,
+                User=self.query_one("#username_field").value,
+                Port=self.query_one("#port_field").value
+            )
+            self.conf.save()
 
 def main():
-    app = SSHConfigEditor()
+    conf = read_ssh_config(expanduser("~/.ssh/config"))
+    app = SSHConfigEditor(conf)
     app.run()
 
 if __name__ == "__main__":
